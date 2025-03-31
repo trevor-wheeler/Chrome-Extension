42f9153d62ead43ae0c5607102433ec2e32de969
trevor-wheeler <contact@trevorwheeler.com>
2025-03-31 00:06:23 -0700

    Added config pages, implemented basic navigation between pages

--- popup.js
+++ popup.js
@@ -1,4 +1,27 @@
 // popup.js
 document.addEventListener('DOMContentLoaded', function() {
-   console.log('Hello world.');
+    const options = document.querySelectorAll('.option-tile');
+
+    const navBtns = document.querySelectorAll('.nav-btn');
+
+    navBtns.forEach(navBtn => {
+        navBtn.addEventListener('click', () => {
+            navigate('home', navBtn.dataset.page);
+        });
+    });
+
+   options.forEach(option => {
+    option.addEventListener('click', () => {
+        const checkbox = option.querySelector('.form-check-input');
+        
+        console.log(checkbox);
+        
+        checkbox.checked = !checkbox.checked;
+    });
+   });
 });
+
+function navigate(close, open) {
+    document.getElementById(close).classList.toggle('open');
+    document.getElementById(open).classList.toggle('open');
+}
